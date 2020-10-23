d68200b2b8f6fe083c3f64b368cc0e3ef2d2f332
Wind <wind@Wind>
2020-10-23 10:43:03 -0500

    aero drag implemented

--- Orbital_Mechanics_with_python/OrbitPropagator.py
+++ Orbital_Mechanics_with_python/OrbitPropagator.py
@@ -6,35 +6,41 @@ from mpl_toolkits.mplot3d import Axes3D
 plt.style.use('dark_background')
 
 import planetary_data as pd
-import tools as t
+import tools as _t
 
-
+d2r=np.pi/180
+r2d=180/np.pi
 
 def null_perts():
     return {
     'J2':False,
     'aero':False,
+    'Cd':0,
+    'A':0,
+    'mu':0,
     'moon gravity':False,
     'solar_gravity':False
+
     }
 
 
 class OrbitPropagator:
     
-    def __init__(self,state0,tspan,dt,coes=False,degres=True,cb=pd.earth, perts=null_perts()):
+    def __init__(self,state0,tspan,dt,coes=False,degres=True,cb=pd.earth, perts=null_perts(),mass0=0):
         
         if coes:
-            self.r0,self.v0,_=t.coes2rv(state0,degres=degres,mu=cb['mu'])
+            self.r0,self.v0,_=_t.coes2rv(state0,degres=degres,mu=cb['mu'])
         else:
             self.r0=state0[:3]
             self.v0=state0[3:]
-
-            
+    
         self.y0=self.r0.tolist()+self.v0.tolist()
         self.tspan=tspan
         self.dt=dt
         self.cb=cb
-                #number of steps
+        self.mass=mass0
+
+        #number of steps
         self.n_steps=int(np.ceil(self.tspan/self.dt))
         
         #initialize arrays
@@ -56,16 +62,25 @@ class OrbitPropagator:
         self.propagate_orbit()
     
     def propagate_orbit(self):
+
+    	print('Propagating orbit...')
    
-        #propagate the orbit
-        while self.solver.successful() and self.step<self.n_steps:
-            self.solver.integrate(self.solver.t+self.dt)
-            self.ts[self.step]=self.solver.t
-            self.ys[self.step]=self.solver.y
-            self.step+=1
-             
-        self.rs=self.ys[:,:3]
-        self.vs=self.ys[:,3:]
+       	#propagate the orbit
+    	while self.solver.successful() and self.step<self.n_steps:
+
+        	#integrate step
+        	self.solver.integrate(self.solver.t+self.dt)
+
+        	#extract values from solver
+        	self.ts[self.step]=self.solver.t
+        	self.ys[self.step]=self.solver.y
+        	self.step+=1
+
+       	#ectract array after the propagation
+    	self.ts=self.ts[:self.step]    
+    	self.rs=self.ys[:self.step,:3]
+    	self.vs=self.ys[:self.step,3:]
+    	self.alts=(np.linalg.norm(self.rs,axis=1)-self.cb['radius']).reshape((self.step,1))
         
     def diffy_q(self,t,y):
     
@@ -91,7 +106,19 @@ class OrbitPropagator:
             a_j2=1.5*self.cb['J2']*self.cb['mu']*self.cb['radius']**2/norm_r**4*np.array([tx,ty,tz])
 
             a+=a_j2
-       
+
+        #aero drag perturbation
+        if self.perts['aero']:
+        	#calculate altitude and air density
+        	z=norm_r-self.cb['radius']
+        	rho=_t.calc_atmospheric_density(z)
+
+        	#calculate motion of s/c with respect to rotating atmosphere
+        	v_rel=v-np.cross(self.cb['atm_rot_vector'],r)
+
+        	drag=-v_rel*0.5*rho*np.linalg.norm(v_rel)*self.perts['Cd']*self.perts['A']/self.mass
+
+        	a+=drag
         
         return [vx,vy,vz,a[0],a[1],a[2]]
 
@@ -104,7 +131,7 @@ class OrbitPropagator:
 
         for n in range (self.n_steps):
 
-            self.coes[n:]=t.rv2coes(self.rs[n,:],self.vs[n,:],mu=self.cb['mu'],degres=degres)
+            self.coes[n:]=_t.rv2coes(self.rs[n,:],self.vs[n,:],mu=self.cb['mu'],degres=degres)
 
     def plot_coes(self,hours=False,days=False,show_plot=False,save_plot=False,title='COEs',figsize=(16,8)):
     	print('Ploting COEs ...')
@@ -145,7 +172,7 @@ class OrbitPropagator:
     	axs[0,1].grid(True)
 
     	#plot argument of periage
-    	axs[0,2].plot(ts,self.coes[:,0])
+    	axs[0,2].plot(ts,self.coes[:,4])
     	axs[0,2].set_title('Argument of Periapse vs.Time')
     	axs[0,2].grid(True)
 
@@ -174,15 +201,15 @@ class OrbitPropagator:
         ax=fig.add_subplot(111,projection='3d')
         
         #plot trayectory and starting point
-        ax.plot(self.rs[:,0],self.rs[:,1],self.rs[:,2],'w',label='Trayectory')
-        ax.plot([self.rs[0,0]],[self.rs[0,1]],[self.rs[0,2]],'wo',label='Initial position')
+        ax.plot(self.rs[:,0],self.rs[:,1],self.rs[:,2],'w',label='Trayectory',zorder=10)
+        ax.plot([self.rs[0,0]],[self.rs[0,1]],[self.rs[0,2]],'wo',label='Initial position',zorder=10)
                
         #plot earth
         _u,_v=np.mgrid[0:2*np.pi:20j,0:np.pi:10j]
         _x=self.cb['radius']*np.cos(_u)*np.sin(_v)
         _y=self.cb['radius']*np.sin(_u)*np.sin(_v)
         _z=self.cb['radius']*np.cos(_v)
-        ax.plot_surface(_x,_y,_z,cmap='Blues')
+        ax.plot_surface(_x,_y,_z,cmap='Blues',zorder=0)
         
         l=self.cb['radius']*2.0
         x,y,z=[[0,0,0],[0,0,0],[0,0,0]]
@@ -207,6 +234,67 @@ class OrbitPropagator:
             plt.show()
         if save_plot:
             plt.savefig(title+'.png',dpi=300)
+
+    def calculate_apoapse_periapse(self):
+    	self.apoapse=self.coes[:,0]*(1+self.coes[:,1])
+    	self.periapse=self.coes[:,0]*(1-self.coes[:,1])
+
+    def plot_apoapse_periapse(self,hours=False,days=False,show_plot=False,save_plot=False,title='Apoapse and Periapse',dpi=500):
+    	#create figure
+    	plt.figure(figsize=(20,10))
+
+    	if hours:
+    		ts=self.ts/3600.0
+    		x_unit='Hours'
+    	elif days:
+    		ts=self.ts/3600/24
+    		x_unit='Days'
+    	else:
+    		ts=self.ts
+    		x_unit='Seconds'
+
+    	#plot each
+    	plt.plot(ts,self.apoapse,'b',label='Apoapse')
+    	plt.plot(ts,self.periapse,'r',label='Periapse')
+
+    	#labels
+    	plt.xlabel('Time(%s)'% x_unit)
+    	plt.ylabel('Altitude (km)')
+
+    	#other parameters
+    	plt.grid(True)
+    	plt.title(title)
+    	plt.legend()
+
+    	if show_plot:
+    		plt.show()
+    	if save_plot:
+    		plt.savefig(title+'.png',dpi=dpi)
+
+    def plot_alts(self,show_plot=False,save_plot=False,hours=False,days=False,title='Radial Distance vs. Time',figsize=(16,8),dpi=500):
+    	#create figure
+    	if hours:
+    		ts=self.ts/3600.0
+    		x_unit='Hours'
+    	elif days:
+    		ts=self.ts/3600/24
+    		x_unit='Days'
+    	else:
+    		ts=self.ts
+    		x_unit='Seconds'
+
+    	plt.figure(figsize=figsize)
+    	plt.plot(ts,self.alts,'w')
+    	plt.grid(True)
+    	plt.xlabel('Time(%s)'% x_unit)
+    	plt.ylabel('Altitude (km)')
+    	plt.title(title)
+    	if show_plot:
+    		plt.show()
+    	if save_plot:
+    		plt.savefig(title+'.png',dpi=dpi)
+
+
             
 
         
---
+++ Orbital_Mechanics_with_python/aero_drag_test.py
@@ -0,0 +1,50 @@
+
+import planetary_data as pd
+import tools as t
+from OrbitPropagator import OrbitPropagator as OP
+from OrbitPropagator import null_perts 
+
+#time parameters
+tspan=3600*24*3
+dt=100.0
+
+#central body
+cb=pd.earth
+
+if __name__ == '__main__':
+
+	perts=null_perts()
+	perts['aero']=True
+	perts['Cd']=2.2
+	perts['A']=(1e-3)**2/4.0 #km²
+
+	#initial mass
+	mass0=10.0 #kg
+
+
+	#perigee and apogee
+	rp=215+cb['radius']
+	ra=300+cb['radius']
+
+	#orbital elements angles
+	raan=340.0
+	i=65.2
+	aop=58.0
+	ta=332.0
+
+	#other orbitaal elements
+	a=(rp+ra)/2.0 #km
+	e=(ra-rp)/(ra+rp)
+
+	#initial state value
+	state0=[a,e,i,ta,aop,raan,191031]
+
+
+	op=OP(state0,tspan,dt,degres=True,coes=True,mass0=mass0,perts=perts)
+
+	op.plot_alts(show_plot=True,hours=True)
+	op.plot_3d(show_plot=True)
+	op.calculate_coes()
+	op.plot_coes(show_plot=True,hours=True)
+	op.calculate_apoapse_periapse()
+	op.plot_apoapse_periapse(show_plot=True,hours=True)
